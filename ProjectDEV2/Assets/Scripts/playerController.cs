da5e053f1df83d0f0db36f0f0d8f801cfa5bb7f4
FG101filmss <logan.k1046@gmail.com>
2025-04-16 17:37:48 -0400

    Crouching and organization
    Added a crouching function and cleaned up the code.

--- ProjectDEV2/Assets/Scripts/playerController.cs
+++ ProjectDEV2/Assets/Scripts/playerController.cs
@@ -14,6 +14,9 @@ public class playerController : MonoBehaviour, IDamage
     [SerializeField] int jumpSpeed;
     [SerializeField] int jumpMax;
     [SerializeField] int gravity;
+    [SerializeField] float crouchHeight;
+    [SerializeField] float standingHeight;
+    [SerializeField] float crouchSpeedMod;
 
     // Shooting
     [SerializeField] float shootRate;
@@ -47,9 +50,8 @@ public class playerController : MonoBehaviour, IDamage
     void Update()
     {
         Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist);
+
         Movement();
-        shootTimer += Time.deltaTime;
-        Shoot();
 
         // HP Bar UI
         hpBarValueText.text = HP.ToString() + "/" + HPOrig.ToString();
@@ -67,14 +69,31 @@ public class playerController : MonoBehaviour, IDamage
             playerVel = Vector3.zero;
         }
 
-        moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
-        controller.Move(moveDir * moveSpeed * Time.deltaTime);
+        moveDir = (Input.GetAxis("Horizontal") * transform.right) +
+                  (Input.GetAxis("Vertical") * transform.forward);
+
+        controller.Move(moveDir * speed * Time.deltaTime);
 
         Jump();
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch(true);
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch(false);
+        }
 
         playerVel.y -= gravity * Time.deltaTime;
         controller.Move(playerVel * Time.deltaTime);
 
+        shootTimer += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && shootTimer >= shootRate)
+        {
+            shoot();
+        }
     }
 
     void Jump()
@@ -86,13 +105,50 @@ public class playerController : MonoBehaviour, IDamage
         }
     }
 
-    void Shoot()
+    void sprint()
     {
-        if (Input.GetButtonDown("Fire1") && shootTimer >= shootRate)
-        {
-            shootTimer = 0;
 
-            Instantiate(projectile, shootPos.position, Camera.main.transform.rotation);
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+
+    void crouch(bool crouch)
+    {
+        isCrouching = crouch;
+
+        controller.height = crouch ? crouchHeight : standingHeight;
+
+        if (crouch)
+        {
+            speed = Mathf.RoundToInt(speed * crouchSpeedMod);
+        }
+        else
+        {
+            speed = Mathf.RoundToInt(speed / crouchSpeedMod);
+        }
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
+        {
+            Debug.Log(hit.collider.name);
+
+            IDamage dmg = hit.collider.GetComponent<IDamage>();
+
+            if (dmg != null)
+            {
+                dmg.takeDamage(shootDamage);
+            }
         }
     }
 
