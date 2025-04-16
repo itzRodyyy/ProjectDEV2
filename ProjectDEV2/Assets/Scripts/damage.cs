04390e5aa8f3d7c61cc8d302d93e5914138e1d1f
itzRodyyy <rodydevs@gmail.com>
2025-04-16 12:45:00 -0700

    Damage Updates
    + Added Lava

--- ProjectDEV2/Assets/Scripts/damage.cs
+++ ProjectDEV2/Assets/Scripts/damage.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class damage : MonoBehaviour
@@ -7,9 +8,12 @@ public class damage : MonoBehaviour
     [SerializeField] Rigidbody rb; 
     
     [SerializeField] int damageAmount;
+    [SerializeField] float damageRate;
     [SerializeField] int speed;
     [SerializeField] int destroyTime;
     [SerializeField] LayerMask self;
+
+    bool isDamaging;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,7 +34,7 @@ public class damage : MonoBehaviour
         {
             Debug.DrawRay(transform.position, transform.forward * speed * Time.deltaTime);
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.forward, out hit, speed * Time.deltaTime, ~self) || Physics.Raycast(transform.position, -transform.forward, out hit, (speed * Time.deltaTime) / 2, ~self))
+            if (Physics.Raycast(transform.position, transform.forward, out hit, speed * Time.deltaTime, ~self) || Physics.Raycast(transform.position, -transform.forward, out hit, (speed * Time.deltaTime) / 4, ~self))
             {
 
                 IDamage dmg = hit.collider.gameObject.GetComponent<IDamage>();
@@ -48,14 +52,38 @@ public class damage : MonoBehaviour
     private void OnTriggerEnter(Collider victim)
     {
         Debug.Log(victim);
-        if (damageType == type.moving)
+        if (damageType != type.DOT)
         {
-            Destroy(gameObject);
+            if (damageType == type.moving || damageType == type.homing)
+            {
+                Destroy(gameObject);
+            }
             IDamage dmg = victim.GetComponent<IDamage>();
             if (dmg != null)
             {
                 dmg.TakeDamage(damageAmount);
             }
         }
+    }
+    private void OnTriggerStay(Collider victim)
+    {
+        if (victim.isTrigger)
+            return;
+        if (damageType == type.DOT)
+        {
+            IDamage dmg = victim.GetComponent<IDamage>();
+            if (dmg != null && !isDamaging)
+            {
+                StartCoroutine(damageOT(dmg));
+            }
+        }
+    }
+
+    IEnumerator damageOT(IDamage d)
+    {
+        isDamaging = true;
+        d.TakeDamage(damageAmount);
+        yield return new WaitForSeconds(damageRate);
+        isDamaging = false;
     }
 }
